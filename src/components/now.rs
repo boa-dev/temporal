c5e5093f07e057a9516951cb0a085c88a7bd8cc5
Kevin Ness <46825870+nekevss@users.noreply.github.com>
2024-12-25 10:38:38 -0600

    Build out the rest of the Now methods

--- src/components/now.rs
+++ src/components/now.rs
@@ -10,18 +10,16 @@ use crate::{iso::IsoDateTime, TemporalUnwrap};
 use super::{
     calendar::Calendar,
     timezone::{TimeZone, TzProvider},
-    EpochNanoseconds, Instant, PlainDateTime,
+    EpochNanoseconds, Instant, PlainDate, PlainDateTime, PlainTime, ZonedDateTime,
 };
+
+#[cfg(feature = "experimental")]
+use crate::{components::timezone::TZ_PROVIDER, TemporalError};
+#[cfg(feature = "experimental")]
+use std::ops::Deref;
 
 /// The Temporal Now object.
 pub struct Now;
-
-impl Now {
-    /// Returns the current time zone.
-    pub fn time_zone_id() -> TemporalResult<String> {
-        sys::get_system_tz_identifier()
-    }
-}
 
 impl Now {
     /// Returns the current instant
@@ -29,16 +27,93 @@ impl Now {
         system_instant()
     }
 
-    pub fn plain_date_time_with_provider(
-        tz: Option<TimeZone>,
-        provider: &impl TzProvider,
-    ) -> TemporalResult<PlainDateTime> {
-        let iso = system_date_time(tz, provider)?;
-        Ok(PlainDateTime::new_unchecked(iso, Calendar::default()))
+    /// Returns the current time zone.
+    pub fn time_zone_id() -> TemporalResult<String> {
+        sys::get_system_tz_identifier()
+    }
+
+    /// Returns the current system time as a `ZonedDateTime` with an ISO8601 calendar.
+    ///
+    /// The time zone will be set to either the `TimeZone` if a value is provided, or
+    /// according to the system timezone if no value is provided.
+    pub fn zoneddatetime_iso(timezone: Option<TimeZone>) -> TemporalResult<ZonedDateTime> {
+        let timezone =
+            timezone.unwrap_or(TimeZone::IanaIdentifier(sys::get_system_tz_identifier()?));
+        let instant = system_instant()?;
+        Ok(ZonedDateTime::new_unchecked(
+            instant,
+            Calendar::default(),
+            timezone,
+        ))
     }
 }
 
-fn system_date_time(
+impl Now {
+    /// Returns the current system time as a `PlainDateTime` with an ISO8601 calendar.
+    ///
+    /// The time zone used to calculate the `PlainDateTime` will be set to either the
+    /// `TimeZone` if a value is provided, or according to the system timezone if no
+    /// value is provided.
+    pub fn plain_datetime_iso_with_provider(
+        timezone: Option<TimeZone>,
+        provider: &impl TzProvider,
+    ) -> TemporalResult<PlainDateTime> {
+        let iso = system_datetime(timezone, provider)?;
+        Ok(PlainDateTime::new_unchecked(iso, Calendar::default()))
+    }
+
+    /// Returns the current system time as a `PlainDate` with an ISO8601 calendar.
+    ///
+    /// The time zone used to calculate the `PlainDate` will be set to either the
+    /// `TimeZone` if a value is provided, or according to the system timezone if no
+    /// value is provided.
+    pub fn plain_date_iso_with_provider(
+        timezone: Option<TimeZone>,
+        provider: &impl TzProvider,
+    ) -> TemporalResult<PlainDate> {
+        let iso = system_datetime(timezone, provider)?;
+        Ok(PlainDate::new_unchecked(iso.date, Calendar::default()))
+    }
+
+    /// Returns the current system time as a `PlainTime` according to an ISO8601 calendar.
+    ///
+    /// The time zone used to calculate the `PlainTime` will be set to either the
+    /// `TimeZone` if a value is provided, or according to the system timezone if no
+    /// value is provided.
+    pub fn plain_time_iso_with_provider(
+        timezone: Option<TimeZone>,
+        provider: &impl TzProvider,
+    ) -> TemporalResult<PlainTime> {
+        let iso = system_datetime(timezone, provider)?;
+        Ok(PlainTime::new_unchecked(iso.time))
+    }
+}
+
+#[cfg(feature = "experimental")]
+impl Now {
+    pub fn plain_datetime_iso(timezone: Option<TimeZone>) -> TemporalResult<PlainDateTime> {
+        let provider = TZ_PROVIDER
+            .lock()
+            .map_err(|_| TemporalError::general("Unable to acquire lock"))?;
+        Now::plain_datetime_iso_with_provider(timezone, provider.deref())
+    }
+
+    pub fn plain_date_iso(timezone: Option<TimeZone>) -> TemporalResult<PlainDate> {
+        let provider = TZ_PROVIDER
+            .lock()
+            .map_err(|_| TemporalError::general("Unable to acquire lock"))?;
+        Now::plain_date_iso_with_provider(timezone, provider.deref())
+    }
+
+    pub fn plain_time_iso(timezone: Option<TimeZone>) -> TemporalResult<PlainTime> {
+        let provider = TZ_PROVIDER
+            .lock()
+            .map_err(|_| TemporalError::general("Unable to acquire lock"))?;
+        Now::plain_time_iso_with_provider(timezone, provider.deref())
+    }
+}
+
+fn system_datetime(
     tz: Option<TimeZone>,
     provider: &impl TzProvider,
 ) -> TemporalResult<IsoDateTime> {
@@ -72,9 +147,9 @@ mod tests {
         let provider = &FsTzdbProvider::default();
         let sleep = 2;
 
-        let before = Now::plain_date_time_with_provider(None, provider).unwrap();
+        let before = Now::plain_datetime_iso_with_provider(None, provider).unwrap();
         thread::sleep(StdDuration::from_secs(sleep));
-        let after = Now::plain_date_time_with_provider(None, provider).unwrap();
+        let after = Now::plain_datetime_iso_with_provider(None, provider).unwrap();
 
         let diff = after.since(&before, DifferenceSettings::default()).unwrap();
 
