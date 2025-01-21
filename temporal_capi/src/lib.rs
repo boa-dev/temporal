96e16b11cb57cd6939e21553a818c779457f86b8
Manish Goregaokar <manishsmail@gmail.com>
2025-01-20 20:45:04 -0800

    Set up basic diplomat workflow (#163)
    Progress on https://github.com/boa-dev/temporal/issues/104
    
    This adds a simple diplomat integration, but does not use it yet. When
    we start using it we should add CI that runs `cargo run -p diplomat-gen`
    and ensures there is no diff.

---
+++ temporal_capi/src/lib.rs
@@ -0,0 +1,8 @@
+#![allow(unused)] // Until we add all the APIs
+#![allow(clippy::needless_lifetimes)] // Diplomat requires explicit lifetimes at times
+
+mod calendar;
+mod error;
+mod options;
+
+mod plain_date;
