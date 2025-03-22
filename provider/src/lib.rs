e119439b6c12a7f99202f0cc40b762942ace321e
Kevin Ness <46825870+nekevss@users.noreply.github.com>
2025-03-22 17:29:06 -0500

    fix lint

--- provider/src/lib.rs
+++ provider/src/lib.rs
@@ -18,20 +18,20 @@ mod tests {
         let iana_normalizer = IanaIdentifierNormalizer::build().unwrap();
         let index = iana_normalizer
             .available_id_index
-            .get(&"America/CHICAGO")
+            .get("America/CHICAGO")
             .unwrap();
         assert_eq!(
             iana_normalizer.normalized_identifiers.get(index),
             Some("America/Chicago")
         );
 
-        let index = iana_normalizer.available_id_index.get(&"uTc").unwrap();
+        let index = iana_normalizer.available_id_index.get("uTc").unwrap();
         assert_eq!(
             iana_normalizer.normalized_identifiers.get(index),
             Some("UTC")
         );
 
-        let index = iana_normalizer.available_id_index.get(&"eTC/uTc").unwrap();
+        let index = iana_normalizer.available_id_index.get("eTC/uTc").unwrap();
         assert_eq!(
             iana_normalizer.normalized_identifiers.get(index),
             Some("Etc/UTC")
