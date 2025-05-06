bbea6e055e10c70d6b3db60e8348965ede0ee2e1
Henrik Tennebekk <henrik.ten@gmail.com>
2025-05-06 23:37:09 +0200

    Update FFI for new parameters in year_month and month_day

--- temporal_capi/src/plain_year_month.rs
+++ temporal_capi/src/plain_year_month.rs
@@ -160,9 +160,9 @@ pub mod ffi {
                 .map(|x| Box::new(Duration(x)))
                 .map_err(Into::into)
         }
-        pub fn to_plain_date(&self) -> Result<Box<PlainDate>, TemporalError> {
+        pub fn to_plain_date(&self, day: Option<u8>) -> Result<Box<PlainDate>, TemporalError> {
             self.0
-                .to_plain_date()
+                .to_plain_date(day)
                 .map(|x| Box::new(PlainDate(x)))
                 .map_err(Into::into)
         }
