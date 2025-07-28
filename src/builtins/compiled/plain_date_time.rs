5ac539a3cc59bf2523feeb88145786b0250744ca
Kevin Ness <nekevss@gmail.com>
2025-07-27 20:35:26 -0500

    Rearrange the PlainDateTime test

--- src/builtins/compiled/plain_date_time.rs
+++ src/builtins/compiled/plain_date_time.rs
@@ -17,3 +17,24 @@ impl PlainDateTime {
         self.to_zoned_date_time_with_provider(time_zone, disambiguation, &*TZ_PROVIDER)
     }
 }
+
+#[cfg(test)]
+mod tests {
+    #[cfg(feature = "tzdb")]
+    #[test]
+    fn to_zoned_date_time_edge_cases() {
+        use crate::{options::Disambiguation, tzdb::CompiledTzdbProvider, PlainDateTime, TimeZone};
+        let provider = &CompiledTzdbProvider::default();
+        let pdt = PlainDateTime::try_new_iso(2020, 3, 8, 2, 30, 0, 0, 0, 0).unwrap();
+        let zdt = pdt
+            .to_zoned_date_time_with_provider(
+                &TimeZone::try_from_identifier_str_with_provider("America/Los_Angeles", provider)
+                    .unwrap(),
+                Disambiguation::Compatible,
+                provider,
+            )
+            .unwrap();
+
+        assert_eq!(zdt.hour(), Ok(3));
+    }
+}
