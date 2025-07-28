feb6656865246ea5bd06292ec1d1160eb8366120
Kevin Ness <46825870+nekevss@users.noreply.github.com>
2025-07-28 02:44:09 -0500

    Fix posix logic that was causing panics (#459)
    Closes #445 and #199.
    
    We were not properly resolving the offset for the POSIX time zone for
    epoch seconds.
    
    This updates the logic to correctly handling determining which offset we
    are in.

--- src/builtins/compiled/plain_date_time.rs
+++ src/builtins/compiled/plain_date_time.rs
@@ -17,3 +17,28 @@ impl PlainDateTime {
         self.to_zoned_date_time_with_provider(time_zone, disambiguation, &*TZ_PROVIDER)
     }
 }
+
+#[cfg(test)]
+mod tests {
+    #[cfg(feature = "tzdb")]
+    #[test]
+    fn to_zoned_date_time_edge_cases() {
+        use crate::{options::Disambiguation, tzdb::CompiledTzdbProvider, PlainDateTime, TimeZone};
+        let provider = &CompiledTzdbProvider::default();
+
+        // Test that a non existent PlainDateTime is successfully disambiguated.
+        //
+        // NOTE(nekevss): POSIX time zone logic of the underlying provider if TZDB is in a "slim" format.
+        let pdt = PlainDateTime::try_new_iso(2020, 3, 8, 2, 30, 0, 0, 0, 0).unwrap();
+        let zdt = pdt
+            .to_zoned_date_time_with_provider(
+                &TimeZone::try_from_identifier_str_with_provider("America/Los_Angeles", provider)
+                    .unwrap(),
+                Disambiguation::Compatible,
+                provider,
+            )
+            .unwrap();
+
+        assert_eq!(zdt.hour(), Ok(3));
+    }
+}
