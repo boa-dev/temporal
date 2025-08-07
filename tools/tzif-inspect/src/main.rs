972cda394f27273bb71f77271a085733156067f9
Manish Goregaokar <manishsmail@gmail.com>
2025-08-07 15:29:43 +0530

    Improve NoLeap/WithLeap handling of tzif-inspect

--- tools/tzif-inspect/src/main.rs
+++ tools/tzif-inspect/src/main.rs
@@ -68,15 +68,17 @@ fn month(m: u16) -> &'static str {
 fn format_transition_day(trans_day: TransitionDay) -> String {
     match trans_day {
         TransitionDay::NoLeap(d) | TransitionDay::WithLeap(d) => {
-            let start_date = PlainDate::new(2001, 1, 1, Default::default()).unwrap();
-            let date_duration = Duration::new(0, 0, 0, (d - 1).into(), 0, 0, 0, 0, 0, 0).unwrap();
+            let no_leap = matches!(trans_day, TransitionDay::NoLeap(_));
+            let start_year = if no_leap { 2001 } else { 2004 };
+            let start_date = PlainDate::new(start_year, 1, 1, Default::default()).unwrap();
+            let date_duration = Duration::new(0, 0, 0, d.into(), 0, 0, 0, 0, 0, 0).unwrap();
             let adjusted = start_date.add(&date_duration, None).unwrap();
             let m = month(adjusted.month().into());
-            let d = adjusted.day();
-            if matches!(trans_day, TransitionDay::NoLeap(_)) {
-                format!("{m} {d} (all years)")
+            let day = adjusted.day();
+            if no_leap {
+                format!("NoLeap({d}): {m} {day}")
             } else {
-                format!("{m} {d} (in non leap years)")
+                format!("WithLeap({d}): {m} {day}")
             }
         }
         TransitionDay::Mwd(m, w, d) => {
