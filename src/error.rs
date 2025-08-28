b7bdf0522662e1252cb3268527274fddf232f206
Manish Goregaokar <manishsmail@gmail.com>
2025-08-28 16:25:16 -0700

    Add ixdtf error strings

--- src/error.rs
+++ src/error.rs
@@ -1,6 +1,7 @@
 //! This module implements `TemporalError`.
 
 use core::fmt;
+use ixdtf::ParseError;
 use timezone_provider::TimeZoneProviderError;
 
 use icu_calendar::DateError;
@@ -136,7 +137,7 @@ impl TemporalError {
         TemporalError::range().with_enum(ErrorMessage::Icu4xDate(error))
     }
 
-    pub(crate) fn from_ixdtf(error: ixdtf::ParseError) -> Self {
+    pub(crate) fn from_ixdtf(error: ParseError) -> Self {
         TemporalError::range().with_enum(ErrorMessage::Ixdtf(error))
     }
 }
@@ -198,7 +199,7 @@ pub(crate) enum ErrorMessage {
     // Typed
     None,
     String(&'static str),
-    Ixdtf(ixdtf::ParseError),
+    Ixdtf(ParseError),
     Icu4xDate(DateError),
 }
 
@@ -242,9 +243,7 @@ impl ErrorMessage {
             }
             Self::None => "",
             Self::String(s) => s,
-            // Todo: produce helpful strings
-            // https://github.com/unicode-org/icu4x/issues/6904
-            Self::Ixdtf(s) => "IXDTF parsing error",
+            Self::Ixdtf(s) => ixdtf_error_to_static_string(s),
             Self::Icu4xDate(DateError::Range { field, .. }) => match field {
                 "year" => "Year out of range.",
                 "month" => "Month out of range.",
@@ -268,6 +267,115 @@ impl From<TimeZoneProviderError> for TemporalError {
             TimeZoneProviderError::Range(s) => Self::range().with_message(s),
             _ => Self::assert().with_message("Unknown TimeZoneProviderError"),
         }
+    }
+}
+
+// ICU4X will get this API natively eventually
+// https://github.com/unicode-org/icu4x/issues/6904
+pub fn ixdtf_error_to_static_string(error: ParseError) -> &'static str {
+    match error {
+        ParseError::ImplAssert => "Implementation error: this error must not throw.",
+
+        ParseError::NonAsciiCodePoint => "Code point was not ASCII",
+
+        ParseError::ParseFloat => "Invalid float while parsing fraction part.",
+
+        ParseError::AbruptEnd { .. } => "Parsing ended abruptly.",
+
+        ParseError::InvalidEnd => "Unexpected character found after parsing was completed.",
+        // Date related errors
+        ParseError::InvalidMonthRange => "Parsed month value not in a valid range.",
+
+        ParseError::InvalidDayRange => "Parsed day value not in a valid range.",
+
+        ParseError::DateYear => "Invalid chracter while parsing year value.",
+
+        ParseError::DateExtendedYear => "Invalid character while parsing extended year value.",
+
+        ParseError::DateMonth => "Invalid character while parsing month value.",
+
+        ParseError::DateDay => "Invalid character while parsing day value.",
+
+        ParseError::DateUnexpectedEnd => "Unexpected end while parsing a date value.",
+
+        ParseError::TimeRequired => "Time is required.",
+
+        ParseError::TimeHour => "Invalid character while parsing hour value.",
+
+        ParseError::TimeMinuteSecond => {
+            "Invalid character while parsing minute/second value in (0, 59] range."
+        }
+
+        ParseError::TimeSecond => "Invalid character while parsing second value in (0, 60] range.",
+
+        ParseError::FractionPart => "Invalid character while parsing fraction part value.",
+
+        ParseError::DateSeparator => "Invalid character while parsing date separator.",
+
+        ParseError::TimeSeparator => "Invalid character while parsing time separator.",
+
+        ParseError::DecimalSeparator => "Invalid character while parsing decimal separator.",
+        // Annotation Related Errors
+        ParseError::InvalidAnnotation => "Invalid annotation.",
+
+        ParseError::AnnotationOpen => "Invalid annotation open character.",
+
+        ParseError::AnnotationClose => "Invalid annotation close character.",
+
+        ParseError::AnnotationChar => "Invalid annotation character.",
+
+        ParseError::AnnotationKeyValueSeparator => {
+            "Invalid annotation key-value separator character."
+        }
+
+        ParseError::AnnotationKeyLeadingChar => "Invalid annotation key leading character.",
+
+        ParseError::AnnotationKeyChar => "Invalid annotation key character.",
+
+        ParseError::AnnotationValueCharPostHyphen => {
+            "Expected annotation value character must exist after hyphen."
+        }
+
+        ParseError::AnnotationValueChar => "Invalid annotation value character.",
+
+        ParseError::InvalidMinutePrecisionOffset => "Offset must be minute precision",
+
+        ParseError::CriticalDuplicateCalendar => {
+            "Duplicate calendars cannot be provided when one is critical."
+        }
+
+        ParseError::UnrecognizedCritical => "Unrecognized annoation is marked as critical.",
+
+        ParseError::TzLeadingChar => "Invalid time zone leading character.",
+
+        ParseError::IanaCharPostSeparator => "Expected time zone character after '/'.",
+
+        ParseError::IanaChar => "Invalid IANA time zone character after '/'.",
+
+        ParseError::UtcTimeSeparator => "Invalid time zone character after '/'.",
+
+        ParseError::OffsetNeedsSign => "UTC offset needs a sign",
+
+        ParseError::MonthDayHyphen => "MonthDay must begin with a month or '--'",
+
+        ParseError::DurationDisgnator => "Invalid duration designator.",
+
+        ParseError::DurationValueExceededRange => {
+            "Provided Duration field value exceeds supported range."
+        }
+
+        ParseError::DateDurationPartOrder => "Invalid date duration part order.",
+
+        ParseError::TimeDurationPartOrder => "Invalid time duration part order.",
+
+        ParseError::TimeDurationDesignator => "Invalid time duration designator.",
+
+        ParseError::AmbiguousTimeMonthDay => "Time is ambiguous with MonthDay",
+
+        ParseError::AmbiguousTimeYearMonth => "Time is ambiguous with YearMonth",
+
+        ParseError::InvalidMonthDay => "MonthDay was not valid.",
+        _ => "General IXDTF parsing error",
     }
 }
 
