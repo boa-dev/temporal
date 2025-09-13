8d2cc25f4e0ad957269746803c52b20014944647
Kevin Ness <nekevss@gmail.com>
2025-09-13 12:18:48 -0500

    Check in ffi bindings

--- temporal_capi/bindings/cpp/temporal_rs/PlainDateTime.d.hpp
+++ temporal_capi/bindings/cpp/temporal_rs/PlainDateTime.d.hpp
@@ -74,12 +74,6 @@ public:
 
   inline static diplomat::result<std::unique_ptr<temporal_rs::PlainDateTime>, temporal_rs::TemporalError> from_utf16(std::u16string_view s);
 
-  inline int32_t iso_year() const;
-
-  inline uint8_t iso_month() const;
-
-  inline uint8_t iso_day() const;
-
   inline uint8_t hour() const;
 
   inline uint8_t minute() const;
