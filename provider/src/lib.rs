211673b1fbd1f4d71939e70290ee01613194a4f7
Kevin Ness <nekevss@gmail.com>
2025-09-13 14:21:30 -0500

    Remove feature gated links

--- provider/src/lib.rs
+++ provider/src/lib.rs
@@ -16,27 +16,26 @@
 //!
 //! Below are a list of currently avaiable time zone providers.
 //!
-//! - [`ZoneInfo64TzdbProvider`][zoneinfo64::ZoneInfo64TzdbProvider] (enable with `zoneinfo64`
-//! features flag)
-//! - [`FsTzdbProvider`][tzif::FsTzdbProvider] (enable with `tzif` feature flag)
-//! - [`CompiledTzdbProvider`][tzif::CompiledTzdbProvider] (enable with `tzif` feature flag)
+//! - `ZoneInfo64TzdbProvider` (enable with `zoneinfo64` features flag)
+//! - `FsTzdbProvider` (enable with `tzif` feature flag)
+//! - `CompiledTzdbProvider` (enable with `tzif` feature flag)
 //!
-//! Coming soon (hopefully), a zero copy compiled tzdb provider (see [experimental_tzif] for more).
+//! Coming soon (hopefully), a zero copy compiled tzdb provider (see `experimental_tzif` for more).
 //!
 //! ## Time zone provider traits
 //!
 //! This crate provides three primary traits for working with time zone data.
 //!
-//! - [`TimeZoneProvider`][provider::TimeZoneProvider]
-//! - [`TimeZoneNormalizer`][provider::TimeZoneNormalizer]
-//! - [`TimeZoneResolver`][provider::TimeZoneResolver]
+//! - [`TimeZoneProvider`][crate::provider::TimeZoneProvider]
+//! - [`TimeZoneNormalizer`][crate::provider::TimeZoneNormalizer]
+//! - [`TimeZoneResolver`][crate::provider::TimeZoneResolver]
 //!
 //! The first trait `TimeZoneProvider` is the primary interface for a time zone provider.
 //!
 //! Meanwhile, the two other traits, `TimeZoneNormalizer` and `TimeZoneResolver`, are secondary
 //! traits that can be used to implement the core `TimeZoneProvider`. Once implemented, this
 //! crate providers a default type for creating a `TimeZoneProvider` from the secondary
-//! traits, [`NormalizerAndResolver`][provider::NormalizerAndResolver].
+//! traits, `NormalizerAndResolver`.
 //!
 //! ### Why two secondary traits?
 //!
