22e64d43f975f91a9cfb44a977c67f05ba599036
Manish Goregaokar <manishsmail@gmail.com>
2026-04-01 20:14:55 -0700

    Update zoneinfo64

--- provider/src/zoneinfo64.rs
+++ provider/src/zoneinfo64.rs
@@ -104,6 +104,7 @@ impl TimeZoneResolver for ZoneInfo64<'_> {
                     },
                 ])
             }
+            _ => return Err(TimeZoneProviderError::Assert("Found unknown PossibleOffset value")),
         };
         Ok(result)
     }
